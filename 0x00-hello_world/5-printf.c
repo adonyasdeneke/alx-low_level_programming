0a2249bcef994335eeb6f0cdca7944e89977e45d
adonyas <denekeadonyas@gmail,com>
2022-12-09 17:38:58 -0500

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,10 @@
 
 /**
  *
+ *
  */
 int main (void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf (with proper grammar, but the outcome is a piece of art,);
 	return (0);
 }
