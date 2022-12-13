8288bd8fefae3a39c82afd8dcea9a8205211dafa
adonyas <denekeadonyas@gmail,com>
2022-12-13 17:23:08 -0500

    ten times lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets in lowercase ten times
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int j = 0;
+
+	while (j < 10)
+	{
+		char i;
+
+		for (i == 'a'; i < 'z'; i++)
+			_putchar(i);
+	}
+		_putchar("\n");
+
+}
