84577d66a5303212e417e8d0ff9b1828e67bcf71
adonyas <denekeadonyas@gmail,com>
2022-12-13 17:33:30 -0500

    ten times lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int j = 0;
+	int j;
+	char i;
 
 	for (j = 0; j < 10; j++)
-	{
-		char i;
-
 		for (i = 'a'; i < 'z'; i++)
-			_putchar(i);
-	}
+			_putchar(i);}
 		_putchar('\n');
 
 }
