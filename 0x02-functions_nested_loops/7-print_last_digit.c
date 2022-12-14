b1200bae2d4868190e7c3acaa57cb2c1b0975b9d
adonyas <denekeadonyas@gmail,com>
2022-12-14 13:13:04 -0500

    last dance

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of the intiger
+ * @n: takes in an int
+ * Return: return value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+	return (x);
+}
