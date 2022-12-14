821a0478a470b6b5c5f11ff92a635ba09e14bfb2
adonyas <denekeadonyas@gmail,com>
2022-12-14 18:46:25 -0500

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - addition of two numbers
+ * @x: number one
+ * @y: number two
+ * Return: return intiger
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	_putchar(sum);
+}
