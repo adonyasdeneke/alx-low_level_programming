978f601f2c2f4ee1633ad49a62930a4babcfe4b4
adonyas <denekeadonyas@gmail,com>
2022-12-26 15:39:27 -0500

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - print string
+ * @str: string
+ *
+ * Return: return nothing
+ */
+void _puts(char *str)
+{
+	int counter = 0;
+
+	while (*(str + counter) != '\0')
+	{
+		if (*(str + counter) != '"')
+		{
+			_putchar(str + counter);
+		}
+		counter++;
+	}
+	_putchar('\n');
+}
