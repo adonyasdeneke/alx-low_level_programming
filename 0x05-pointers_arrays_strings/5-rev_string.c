6738ec6718ea332e3bb6951db922a8959566f062
adonyas <denekeadonyas@gmail,com>
2022-12-29 17:54:01 -0500

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,9 +7,15 @@ void rev_string(char *s)
 {
 	int temp = 0, c = 0, i;
 
+	while (*(s + c) != '\0')
+	{
+		c++;
+	}
+
 	for (i = 0; i <= ((c - 1) / 2); i++)
 	{
 		temp = *(s + i);
-		*(s + i) = *(s + c - i);
-		*(s + c - i) = temp;
+		*(s + i) = *(s + (c - (i + 1)));
+		*(s + (c - (i + 1))) = temp;
 	}
+}
