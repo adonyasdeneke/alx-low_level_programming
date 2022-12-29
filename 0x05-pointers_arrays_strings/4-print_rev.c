db042c4312fb20bd4de3f4edef44abf162406db4
adonyas <denekeadonyas@gmail,com>
2022-12-29 16:46:13 -0500

    revers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print string in reverse
+ * @s: pointer to the string
+ *
+ */
+void print_rev(char *s)
+{
+	int c1, c2;
+
+	while (*(s + c1) != '\0')
+	{
+		c1++;
+	}
+	c2 = c1;
+	while (c1 >= 0)
+		if (*(s + c) != '"' && c2 != c1)
+		{
+			_putcahr(*(s + c1));
+		}
+	-putchar('\n');
+}
