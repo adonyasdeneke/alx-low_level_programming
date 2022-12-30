14405a75f7eb723efbfce6d5dedff83af1feafd8
adonyas <denekeadonyas@gmail,com>
2022-12-30 11:43:31 -0500

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (succes)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	printf("\n");
+	return (0);
+}
+
