9e793062668c10e2e64f2eac0f624f66c2ce9af2
adonyas <denekeadonyas@gmail,com>
2023-01-13 03:04:00 -0500

    combinatio

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: return zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 49; i <= 57; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}
